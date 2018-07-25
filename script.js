215f219f60d74a280bdc97a0bef7735afd8a5e47
Kang-Kyu Lee <kangkyu1111@gmail.com>
2018-07-25 14:07:14 -0700

    Refactor: pull out as a function and use callbacks

--- script.js
+++ script.js
@@ -2,18 +2,25 @@ var addInput = document.getElementById('todo-add-field');
 var addButton = document.getElementById('todo-add-button');
 var ul = document.getElementsByTagName('ul')[0];
 
-addButton.addEventListener('click', function() {
-  if (addInput.value.length > 0) {
-    var li = document.createElement('li')
-    li.appendChild(document.createTextNode(addInput.value));
-    ul.appendChild(li);
-  }
-});
+function addItem() {
+  var li = document.createElement('li')
+  li.appendChild(document.createTextNode(addInput.value));
+  ul.appendChild(li);
+  addInput.value = '';
+}
 
-addInput.addEventListener('keypress', function(e) {
-  if (addInput.value.length > 0 && e.keyCode === 13) {
-    var li = document.createElement('li')
-    li.appendChild(document.createTextNode(addInput.value));
-    ul.appendChild(li);
+var addItemByButtonClick = function() {
+  if (addInput.value.length > 0) {
+    addItem();
   }
-});
+}
+
+addButton.addEventListener('click', addItemByButtonClick);
+
+var addItemByEnter = function(e) {
+  if (addInput.value.length > 0 && e.keyCode === 13) {
+    addItem();
+  }
+}
+
+addInput.addEventListener('keypress', addItemByEnter);
