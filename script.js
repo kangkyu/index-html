08d2095960b9613902ab297c2e1bad038a91bd1f
Kang-Kyu Lee <kangkyu1111@gmail.com>
2018-07-25 14:05:09 -0700

    Add shopping items with button click or press enter

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
-var button = document.getElementsByTagName('button')[0];
+var addInput = document.getElementById('todo-add-field');
+var addButton = document.getElementById('todo-add-button');
+var ul = document.getElementsByTagName('ul')[0];
 
-button.addEventListener('click', function() {
-  console.log('Click!!!')
+addButton.addEventListener('click', function() {
+  if (addInput.value.length > 0) {
+    var li = document.createElement('li')
+    li.appendChild(document.createTextNode(addInput.value));
+    ul.appendChild(li);
+  }
+});
+
+addInput.addEventListener('keypress', function(e) {
+  if (addInput.value.length > 0 && e.keyCode === 13) {
+    var li = document.createElement('li')
+    li.appendChild(document.createTextNode(addInput.value));
+    ul.appendChild(li);
+  }
 });
